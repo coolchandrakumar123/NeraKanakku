69c05743783628f21682473690925591453c53cd
chandrakumar.selvaraj <chandrakumar.selvaraj@zohocorp.com>
2018-02-23 16:55:08 +0530

    NeraKanakku Initial Version

--- app/src/main/java/com/chan/nerakanakku/timer/TimerPresenter.java
+++ app/src/main/java/com/chan/nerakanakku/timer/TimerPresenter.java
@@ -95,6 +95,26 @@ public class TimerPresenter implements TimerContract.Presenter
                 }, 0);
             }
         }
+
+        private String convertMilliSeconds(long milliSeconds)
+        {
+            int seconds = (int) (milliSeconds / 1000);
+            int minutes = seconds / 60;
+            int hours = seconds / (60 * 60);
+            seconds = seconds % 60;
+            if(hours == 0 && minutes == 0)
+            {
+                return (String.format("00:00:%02d", seconds)); //No I18N
+            }
+            else if(hours == 0)
+            {
+                return (String.format("00:%02d:%02d", minutes, seconds)); //No I18N
+            }
+            else
+            {
+                return (String.format("%02d:%02d:%02d", hours, minutes, seconds)); //No I18N
+            }
+        }
     }
 
 }
